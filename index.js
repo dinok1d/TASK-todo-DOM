40084a1d8faf6ee45e9ef9fed226d02726f81461
Shahad Al Mashaan <shahad@Shahads-MacBook-Pro-2.local>
2021-09-29 22:13:37 +0300

    geetting

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const { values } = require("core-js/core/array");
+
 // You do not need to change the constants
 const CATEGORY_SELECTOR = "categories-list";
 const CATEGORY_FILTER = "categories-list-filter";
 
 let tasks = [];
-let obj = {};
-obj.id = 0;
-obj.done = "false";
+// let obj = {};
+// // obj.id = 0;
+// obj.done = false;
 
 let categories = [];
 
@@ -18,37 +20,55 @@ let categories = [];
 // console.log(tasks)
 // categories = ["Movies", "Groceries"];
 // SAMPLE
+
 renderCategories(categories, CATEGORY_SELECTOR);
 renderCategories(categories, CATEGORY_FILTER);
 renderTasks(tasks, "tasks-list");
 
 function taskChecked(taskId, checked) {
   // implement the delete task.
-  // You are given the task id
+  // // You are given the task id
+
+  // tasks.forEach((id) => {
+  //   id = !!false;
+  // });
+
+  console.log(checked);
   console.log(`${checked ? "" : "UN"}CHECKED TASK`, taskId);
 }
 
 function addTask() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
   const taskTitle = getNewTaskText();
-  obj.title = `${taskTitle}`;
-  obj.category = `${selectedCategory}`;
+  const obj = {
+    id: tasks.length,
+    title: taskTitle,
+    category: selectedCategory,
+    done: false,
+  };
   tasks.push(obj);
   renderTasks(tasks, "tasks-list");
-  alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
+  // alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
 }
 
 function addCategory() {
   const newCategory = getNewCategoryText();
   categories.push(newCategory);
   renderCategories(categories, CATEGORY_SELECTOR);
-  alert(`New category was added: ${newCategory}`);
+  // alert(`New category was added: ${newCategory}`);
 }
 
 function filterTasks() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_FILTER);
   const done = getFilteredDone();
+  // categories.push(selectedCategory);
+  renderCategories(categories, CATEGORY_FILTER);
   // continue the code here
   // REMOVE ME: sample alert
   alert(`Category: ${selectedCategory} | done: ${done}`);
 }
+
+// function idIncrement() {
+//   if taskTitle()
+//   return id + 1;
+// }
