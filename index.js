a6e1dfedced12183ea762bb8a309e9a222a86a0f
Shahad Al Mashaan <shahad@Shahads-MacBook-Pro-2.local>
2021-09-29 20:30:10 +0300

    yay

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const CATEGORY_SELECTOR = "categories-list";
 const CATEGORY_FILTER = "categories-list-filter";
 
 let tasks = [];
+let obj = {};
+obj.id = 0;
+obj.done = "false";
+
 let categories = [];
 
-// REMOVE ME: SAMPLE FILLING
-tasks = [
-  { id: 0, title: "Game of thrones", category: "Movies", done: false },
-  { id: 1, title: "Toy Story 4", category: "Movies", done: false },
-];
-console.log(tasks)
-categories = ["Movies", "Groceries"];
+// // REMOVE ME: SAMPLE FILLING
+// tasks = [
+//   { id: 0, title: "Game of thrones", category: "Movies", done: false },
+//   { id: 1, title: "Toy Story 4", category: "Movies", done: false },
+// ];
+
+// console.log(tasks)
+// categories = ["Movies", "Groceries"];
 // SAMPLE
 renderCategories(categories, CATEGORY_SELECTOR);
 renderCategories(categories, CATEGORY_FILTER);
@@ -26,13 +31,17 @@ function taskChecked(taskId, checked) {
 function addTask() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
   const taskTitle = getNewTaskText();
-  // continue the code here
+  obj.title = `${taskTitle}`;
+  obj.category = `${selectedCategory}`;
+  tasks.push(obj);
+  renderTasks(tasks, "tasks-list");
   alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
 }
 
 function addCategory() {
   const newCategory = getNewCategoryText();
-  // continue the code here
+  categories.push(newCategory);
+  renderCategories(categories, CATEGORY_SELECTOR);
   alert(`New category was added: ${newCategory}`);
 }
 
